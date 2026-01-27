094e4f3c599934c6b54d0db55d2b8f625faea0e8
mac <85289658+85406043@users.noreply.github.com>
2026-01-27 09:33:41 -0300

    Update exe5.py

---
+++ Estudying/Python/World 1/exe5.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number: "))
+ant_number = number - 1
+suc_number = number + 1
+print("The successor: {}".format(suc_number))
+print("The predecessor: {}".format(ant_number))
+
