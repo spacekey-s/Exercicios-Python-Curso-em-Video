55db4d2d09b655d148ef7fbda82d24f25efbf56f
mac <85289658+85406043@users.noreply.github.com>
2026-01-30 09:35:28 -0300

    Create exe13.py

---
+++ Estudying/Python/World 1/exe13.py
@@ -0,0 +1,8 @@
+# i need a salary
+salary = float(input("What is your salary? R$"))
+# calculate(rule of three)
+rule_of_three = (salary * 15) /100
+# calculate the increase
+new_salary = salary + rule_of_three
+# show print
+print("Your new salary is R${}".format(new_salary))
