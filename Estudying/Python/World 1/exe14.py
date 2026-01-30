4e8dee6602e065cb016ca2998598ff00b7f5a38d
mac <85289658+85406043@users.noreply.github.com>
2026-01-30 09:35:46 -0300

    Create exe14.py

---
+++ Estudying/Python/World 1/exe14.py
@@ -0,0 +1,7 @@
+#formule => F = (C × 1,8) + 32
+#fahrenheit
+celsius  = float (input("Convert the degrees to C°: "))
+#calculate to celsius
+fahrenheit = (celsius * 1.8) + 32
+#show
+print("The degrees given, converted to Fahrenheit is {:.1f}F.".format(fahrenheit))
