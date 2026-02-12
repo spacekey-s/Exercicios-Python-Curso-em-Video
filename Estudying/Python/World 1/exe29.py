b160042614afe235cca66daa657e3f41773e36ac
mac <85289658+85406043@users.noreply.github.com>
2026-02-12 06:47:56 -0300

    Update exe29.py

--- Estudying/Python/World 1/exe29.py
+++ Estudying/Python/World 1/exe29.py
@@ -1,5 +1,3 @@
-"""Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite."""
-
 #i need km
 velocityCar = float(input("Enter the car's speed: "))
 #conditions
@@ -14,5 +12,3 @@ else:
   #show result
   print(f"You exceeded the average by {kmExceeded}KM above (80KM standard).")
   print(f"Thefore, your fine will be R${calcFine}.")
-
-
