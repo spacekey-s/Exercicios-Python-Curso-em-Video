95e1fdfd98f1789e4be3b9ae2b0314f2d2bb4c01
Felipe Pereira Cunha <85289658+85406043@users.noreply.github.com>
2026-03-09 15:03:31 -0300

    Comment out mouse click actions in main_autogui.py
    Comment out click actions and add instructions for usage.

--- Automações/main_autogui.py
+++ Automações/main_autogui.py
@@ -5,18 +5,18 @@ import pyperclip
 tp.sleep(3)
 pat.press("win")
 tp.sleep(2)
-pat.typewrite("TikTok", interval=0.5)
+pat.typewrite("TikTok", interval=0.5) # coloque o nome do app
 tp.sleep(1)
 pat.press("enter")
 tp.sleep(20)
-pat.click(100, 275, duration=1) # Use o mouseinfo para colocar as dimensões dos clicks na tela
+#pat.click(100, 275, duration=1) # Use o mouseinfo para colocar as dimensões dos clicks na tela | descomente se for usar
 tp.sleep(5)
-pat.click(539, 304, duration=1) # Use o mouseinfo para colocar as dimensões dos clicks na tela
+#pat.click(539, 304, duration=1) # Use o mouseinfo para colocar as dimensões dos clicks na tela | descomente se for usar
 tp.sleep(5)
 
 for i in range(50):
 
-    pat.click(1481, 988, duration=1) # Use o mouseinfo para colocar as dimensões dos clicks na tela
+    #pat.click(1481, 988, duration=1) # Use o mouseinfo para colocar as dimensões dos clicks na tela | descomente se for usar
     tp.sleep(1)
     pyperclip.copy("Coloque Seu texto")
     tp.sleep(1)
@@ -24,6 +24,6 @@ for i in range(50):
     tp.sleep(1)
     pat.press("Enter")
     tp.sleep(2)
-    pat.click(1148, 586, duration=1);tp.sleep(1) # Use o mouseinfo para colocar as dimensões dos clicks na tela
+    #pat.click(1148, 586, duration=1);tp.sleep(1) # Use o mouseinfo para colocar as dimensões dos clicks na tela | descomente se for usar
     pat.hotkey("down")
     tp.sleep(5)
