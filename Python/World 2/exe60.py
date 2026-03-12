b84eee4eca34564d7e449087ae07b8707e08f403
mac <85289658+85406043@users.noreply.github.com>
2026-03-12 17:16:10 -0300

    Update exe60.py

--- Python/World 2/exe60.py
+++ Python/World 2/exe60.py
@@ -1,1 +1,16 @@
+#Maneira com módulos atuais
+"""import math
 
+
+numberFact = int(input("Digite um número: "))
+
+factorial = math.factorial(numberFact)
+
+print(f"O factorial do número {numberFact} é {factorial}.")"""
+
+numberFact = int(input("Digite um número: "))
+counter = numberFact
+
+while counter > 0:
+  counter -= 1
+  print(counter)
