86fdd3a1285198463b6739507d07b07f0aea72e0
Felipe  Cunha <85289658+spacekey-s@users.noreply.github.com>
2026-03-17 16:55:11 -0300

    Update exe65.py

--- Python/World 2/exe65.py
+++ Python/World 2/exe65.py
@@ -1,14 +1,14 @@
-"""Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
-"""
-
+#variables
 condicao = 0
 listNumbers = []
 
+#loop
 while condicao == 0:
   numbers = int(input("Digite um número: "))
   listNumbers.append(numbers)
   question = str(input("Deseja continuar ? [S/N] -> ")).strip().upper()
-
+  
+  #condition
   if question == "N":
     media = sum(listNumbers) / len(listNumbers)
     print(f"A média na soma dos valores é {media}.")
