ef1af3e6e615618c8f758c64b0924132c9921ec5
mac <85289658+85406043@users.noreply.github.com>
2026-03-25 21:11:41 -0300

    Update exe68.py

--- Python/World 2/exe68.py
+++ Python/World 2/exe68.py
@@ -1,3 +1,6 @@
 """Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo."""
 
-
+import random
+numberUser = int(input("Escolha um número inteiro: "))
+computador = random.randint(0, 100)
+print(computador)
