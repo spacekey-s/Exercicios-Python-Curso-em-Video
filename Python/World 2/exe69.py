4d7c2cd6f10041207ab0bd9472ad048f9475179f
mac <85289658+spacekey-s@users.noreply.github.com>
2026-03-31 20:12:13 -0300

    Update exe69.py

--- Python/World 2/exe69.py
+++ Python/World 2/exe69.py
@@ -6,9 +6,11 @@ B) quantos homens foram cadastrados.
 
 C) quantas mulheres tem menos de 20 anos."""
 import time
+from colorama import Fore, Style
 
 listUsers = []
 listMaior = []
+listMulher = []
 
 print("-="*25)
 print("Cadastro de Pessoas")
@@ -18,18 +20,25 @@ while True:
   ageUser = int(input("Digite sua idade: "));listUsers.append(ageUser)
   sexUser = str(input("Digite seu sexo [M/F]: ")).replace(" ", "").upper();listUsers.append(sexUser)
 
-  #condição para a lista maior
+  #condição para a listMaior
   if ageUser > 18:
     listMaior.append(sexUser)
+  #condição para listMulher
+  if ageUser < 20 and sexUser == "F":
+    listMulher.append(sexUser)
 
-  print("-"*25)
-  print("Cadastro concluído!")
-  print("-"*25)
+  print("-"*19)
+  print(Fore.GREEN + "Cadastro concluído!" + Style.RESET_ALL)
+  print("-"*19)
 
   condStop = str(input("Você deseja continuar? [S/N] => ")).strip().upper()
+  print("-="*25)
 
   #condição de parada
   if "N" in condStop:
     print("Finalizando...")
     time.sleep(3)
+    print(f"{len(listMaior)} pessoa(s) com idade superior a 18 anos.")
+    print(f"Quantidade de {listUsers.count("M")} pessoa(s), do sexo masculino. ")
+    print(f"{len(listMulher)} mulhere(s), com idade inferior a 20 anos.")
     break
