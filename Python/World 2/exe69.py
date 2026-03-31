813820c53f563f7378bb7293aae88bc1672345d7
mac <85289658+spacekey-s@users.noreply.github.com>
2026-03-31 19:48:56 -0300

    Update exe69.py

--- Python/World 2/exe69.py
+++ Python/World 2/exe69.py
@@ -7,16 +7,29 @@ B) quantos homens foram cadastrados.
 C) quantas mulheres tem menos de 20 anos."""
 import time
 
+listUsers = []
+listMaior = []
+
 print("-="*25)
 print("Cadastro de Pessoas")
 print("-="*25)
 
 while True:
-  ageUser = int(input("Digite sua idade: "))
-  sexUser = str(input("Digite seu sexo: "))
+  ageUser = int(input("Digite sua idade: "));listUsers.append(ageUser)
+  sexUser = str(input("Digite seu sexo [M/F]: ")).replace(" ", "").upper();listUsers.append(sexUser)
+
+  #condição para a lista maior
+  if ageUser > 18:
+    listMaior.append(sexUser)
+
+  print("-"*25)
+  print("Cadastro concluído!")
+  print("-"*25)
+
   condStop = str(input("Você deseja continuar? [S/N] => ")).strip().upper()
-  #condição
-  if "S" in condStop:
+
+  #condição de parada
+  if "N" in condStop:
     print("Finalizando...")
     time.sleep(3)
     break
