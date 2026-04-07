28fd70aeb40018e810ed0a841d5607c5a0a1a746
mac <85289658+spacekey-s@users.noreply.github.com>
2026-04-07 18:55:57 -0300

    Update exe71.py

--- Python/World 2/exe71.py
+++ Python/World 2/exe71.py
@@ -2,3 +2,28 @@
 
 considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1."""
 
+valor = int(input("Valor a ser sacado(int number): R$"))
+
+#NOTAS 50
+if valor >= 50:
+  notas_50 = valor // 50
+  print(f"{notas_50} nota(s) de R$50.")
+  valor = valor % 50
+
+#NOTAS 20
+if valor >= 20:
+  notas_20 = valor // 20
+  print(f"{notas_20} nota(s) de R$20.")
+  valor = valor % 20
+
+#NOTAS 10
+if valor >= 10:
+  notas_10 = valor // 10
+  print(f"{notas_10} nota(s) de R$10.")
+  valor = valor % 10
+
+#NOTAS 1
+if valor >= 1:
+  notas_1 = valor // 1
+  print(f"{notas_1} nota(s) de R$1.")
+  valor = valor % 1
